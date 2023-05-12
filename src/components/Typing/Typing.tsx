b560b04c11d9e5c8e6a444db03e8cda3f7e7d31f
Ian Nordby <ian.c.nordby@gmail.com>
2023-05-12 15:41:39 -0600

    Add basic validation to inputs it the next character input matches the next character in the example text

--- src/components/Typing/Typing.tsx
+++ src/components/Typing/Typing.tsx
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 
 const Typing: React.FC = () => {
-  const [textInput, setTextInput] = useState('')
+  const [textInput, setTextInput] = useState("");
 
-  const example = 'You must me a pretty tough fighter to have made it past my cow!'
+  const example =
+    "You must me a pretty tough fighter to have made it past my cow!";
+
+  const validateInputs = (inputValue: string) => {
+    const exampleArr = example.split("");
+    console.log(inputValue);
+    if (inputValue.split("").every((el, index) => exampleArr[index] === el)) {
+      setTextInput(inputValue);
+    }
+  };
 
   return (
     <div>
       <p>{example}</p>
-      <p>{textInput || 'hi'}</p>
-      <input value={textInput} onChange={e => setTextInput(e.target.value)}/>
+      <span>{textInput}</span>
+      <span className="missing">{example.substring(textInput.length)}</span>
+      <input
+        value={textInput}
+        onChange={(e) => validateInputs(e.target.value)}
+      />
     </div>
   );
 };
