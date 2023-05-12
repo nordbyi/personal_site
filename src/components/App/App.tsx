18c9d81347bff78caa7909e29edbc5beb90d2ddd
Ian Nordby <ian.c.nordby@gmail.com>
2023-05-12 15:26:22 -0600

    Import Typing Component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Typing from "../Typing/Typing";
 import "./App.css";
 
 const App: React.FC = () => {
-  return <div className="App">Hello!</div>;
+  return (
+    <div className="App">
+      <Typing />
+    </div>
+  );
 };
 
 export default App;
