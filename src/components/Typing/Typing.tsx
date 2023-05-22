207df23704f3b94a2cf4ed7b7dcc85b975215ef5
Ian Nordby <ian.c.nordby@gmail.com>
2023-05-22 17:14:51 -0600

    Clean up code

--- src/components/Typing/Typing.tsx
+++ src/components/Typing/Typing.tsx
@@ -1,5 +1,7 @@
-import React, { FormEvent, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import DisplayText from "./DisplayText/DisplayText";
 import "./Typing.css";
+
 
 const Typing: React.FC = () => {
   const [textInput, setTextInput] = useState<string>("");
@@ -9,17 +11,17 @@ const Typing: React.FC = () => {
     "You must be a pretty tough fighter to have made it past my cow!";
 
   const validateInputs = (inputValue: string) => {
-    if(win) return
+    if (win) return;
     const exampleArr = example.split("");
     // console.log(inputValue);
-    
+
     if (inputValue.split("").every((el, index) => exampleArr[index] === el)) {
       setTextInput(inputValue);
     } else {
-      // make wrong letter appear on screen where letter was attempted to be added. 
+      // make wrong letter appear on screen where letter was attempted to be added.
+      // create a component made with useMemo to preserve it's values through rerenders?
+      // store them inside the as an array to survive state rerenders?
     }
-
-
   };
 
   useEffect(() => {
@@ -33,15 +35,16 @@ const Typing: React.FC = () => {
   return (
     <div>
       <div>
-        <span className="correct">{textInput}</span>
-        <span className="error">test</span>
+        <DisplayText example={example} current={textInput} />
+        {/* <span className="correct">{textInput}</span>
+        <span className="incorrect">{}</span> */}
         {/* Make error letter component that pops out wrong letters (at random directions?) 
         when pressed with animations that fade after a second or so  */}
-        <span className="missing">{example.substring(textInput.length)}</span>
+        {/* <span className="missing">{example.substring(textInput.length)}</span> */}
       </div>
       <input
         value={textInput}
-        onChange={(e) => validateInputs(e.target.value)}
+        onChange={(e) => setTextInput(e.target.value)}
       />
       {win && <p>Yay, you win!</p>}
     </div>
