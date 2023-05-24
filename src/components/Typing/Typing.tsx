58882db2766d9cdca8257dc8df50b87fc902ef67
Ian Nordby <ian.c.nordby@gmail.com>
2023-05-24 09:36:51 -0600

    Clean up comments and console.logs

--- src/components/Typing/Typing.tsx
+++ src/components/Typing/Typing.tsx
@@ -13,30 +13,6 @@ const Typing: React.FC = () => {
   const example =
     "You must be a pretty tough fighter to have made it past my cow!";
 
-  // const validateInputs = (inputValue: string) => {
-  //   if (win) return;
-  //   const exampleArr = example.split("");
-  //   // console.log(inputValue);
-
-  //   if (inputValue.split("").every((el, index) => exampleArr[index] === el)) {
-  //     setTextInput(inputValue);
-  //   } else {
-  //     // make wrong letter appear on screen where letter was attempted to be added.
-  //     // create a component made with useMemo to preserve it's values through rerenders?
-  //     // store them inside the as an array to survive state rerenders?
-  //   }
-  // };
-
-  // const handleKeydown = (event: KeyboardEvent, input: string): void => {
-  //   console.log(event.key)
-  //   if (example[input.length] === event.key) {
-  //     setTextInput(input + event.key)
-  //   } else {
-  //     document.addEventListener('keydown', (event) => {
-  //       handleKeydown(event, textInput)}, {once: true})
-  //     }
-  // }
-
   const handleKeydown = (event: KeyboardEvent, input: string, win: boolean): void => {
     if (win) return
     console.log(event.key)
@@ -51,20 +27,15 @@ const Typing: React.FC = () => {
   }
 
   console.log('text input', textInput)
-  console.log('text input length', textInput.length)
 
   useEffect(() => {
     if(textInput === example) {
       setWin(true);
-      // disable game area
       return
     }
 
     document.addEventListener('keydown', (event) => {
       handleKeydown(event, textInput, win)}, {once: true})
-    
-    return () => {document.removeEventListener('keydown', (event) => {
-      handleKeydown(event, textInput, win)})}
   }, [textInput])
 
   return (
@@ -73,7 +44,6 @@ const Typing: React.FC = () => {
         <DisplayText example={example} current={textInput} />
         {/* Make error letter component that pops out wrong letters (at random directions?) 
         when pressed with animations that fade after a second or so  */}
-        {/* <span className="missing">{example.substring(textInput.length)}</span> */}
       </div>
       {win && <p>Yay, you win!</p>}
     </div>
