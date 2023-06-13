75abbb905ce878e39c6c2cc76b0bdfdc78e61f4a
Ian Nordby <ian.c.nordby@gmail.com>
2023-06-13 11:33:17 -0600

    Conditionally render either game-slider div or game-over div

--- src/components/TimerDisplay/TimerDisplay.tsx
+++ src/components/TimerDisplay/TimerDisplay.tsx
@@ -19,10 +19,10 @@ const TimerDisplay: React.FC<Props> = ({time, win, loss, updateGameLoss}) => {
 
   return (
     <div className='timer-container'>
-      <div className='timer-slider' style={{width: width, transition: `all ${time}s linear`}}>
-        {!win && !loss && <GameTimer time={time} win={win} loss={loss} updateGameLoss={updateGameLoss} />}
-      </div>
-      <span></span>
+      {!win && !loss && <div className='timer-slider' style={{width: width, transition: `all ${time}s linear`}}>
+        <GameTimer time={time} win={win} loss={loss} updateGameLoss={updateGameLoss} />
+      </div>}
+      {win || loss && <div className={`game-over ${win ? 'win' : 'loss'}`}></div>}
     </div>
   );
 };
