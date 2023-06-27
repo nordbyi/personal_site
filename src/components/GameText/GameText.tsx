89ec480823b1611ab01ce8ecaad441ac100a2cf9
Ian Nordby <ian.c.nordby@gmail.com>
2023-06-27 11:05:18 -0600

    Add useState and useEffect to make a typewritting effect to display game text

--- src/components/GameText/GameText.tsx
+++ src/components/GameText/GameText.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 interface Props {
   text: string
 }
 
 const GameText:React.FC<Props> = ({text}) => {
-  return <p>{text}</p>
+  const [textIndex, setTextIndex] = useState<number>(0)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTextIndex(textIndex + 1)
+    }, 100)
+  }, [textIndex])
+
+  return <p>{text.substring(0, textIndex)}</p>
 }
 
 export default GameText
