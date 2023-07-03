c283816cb4c9c16989a2f8b88f8fdf3c12f2b772
Ian Nordby <ian.c.nordby@gmail.com>
2023-07-03 11:57:12 -0600

    Add placeholder src link

--- src/components/CharacterImage/CharacterImage.tsx
+++ src/components/CharacterImage/CharacterImage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CharacterImage:React.FC = () => {
-  return <h1>character image</h1>
+  return <img src="https://user-images.githubusercontent.com/108428451/227247013-357061a7-8b34-4cb3-a2e6-f2eaed388a52.gif"/>
 }
 
 export default CharacterImage
