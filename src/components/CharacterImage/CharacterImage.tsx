692341af7daca1c4c2f3add223e6d1b590921b18
Ian Nordby <ian.c.nordby@gmail.com>
2023-07-03 12:05:18 -0600

    Refactor component to receive prop from parent with the image's src value

--- src/components/CharacterImage/CharacterImage.tsx
+++ src/components/CharacterImage/CharacterImage.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import './CharacterImage.css'
 
-const CharacterImage:React.FC = () => {
-  return <img className="character-img" src="https://user-images.githubusercontent.com/108428451/227247013-357061a7-8b34-4cb3-a2e6-f2eaed388a52.gif"/>
+interface Props {
+  source: string
+}
+
+const CharacterImage:React.FC<Props> = ({source}) => {
+  return <img className="character-img" src={source}/>
 }
 
 export default CharacterImage
