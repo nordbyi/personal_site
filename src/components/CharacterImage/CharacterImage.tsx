e4b299e9ce2b53d19b59267a9f77e1e614864adc
Ian Nordby <ian.c.nordby@gmail.com>
2023-07-03 12:00:42 -0600

    Add css file import and className to image element

--- src/components/CharacterImage/CharacterImage.tsx
+++ src/components/CharacterImage/CharacterImage.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import './CharacterImage.css'
 
 const CharacterImage:React.FC = () => {
-  return <img src="https://user-images.githubusercontent.com/108428451/227247013-357061a7-8b34-4cb3-a2e6-f2eaed388a52.gif"/>
+  return <img className="character-img" src="https://user-images.githubusercontent.com/108428451/227247013-357061a7-8b34-4cb3-a2e6-f2eaed388a52.gif"/>
 }
 
 export default CharacterImage
