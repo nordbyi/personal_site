a01f9e1bb04e47185d81a42c29c83610ff9210ba
Ian Nordby <ian.c.nordby@gmail.com>
2023-07-03 17:27:52 -0600

    Add useState to provide initial false value for fadeValue so that it updates properly on mount and fade prop update

--- src/components/CharacterImage/CharacterImage.tsx
+++ src/components/CharacterImage/CharacterImage.tsx
@@ -1,17 +1,19 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import './CharacterImage.css'
 
 interface Props {
   source: string,
-  fade: string
+  fade: boolean
 }
 
 const CharacterImage:React.FC<Props> = ({source, fade}) => {
+  const [fadeValue, setFadeValue] = useState<boolean>(false)
+
   useEffect(() => {
+    setFadeValue(fade)
+  }, [fade])
 
-  }, [source])
-
-  return <img className={`character-img fade-${fade}`} src={source}/>
+  return <img className={`character-img ${fadeValue ? 'fade-in' : 'fade-out'}`} src={source}/>
 }
 
 export default CharacterImage
