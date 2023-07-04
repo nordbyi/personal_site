a9e0aae976105b052a23101b8b3d3e3c12e28446
Ian Nordby <ian.c.nordby@gmail.com>
2023-07-03 18:46:25 -0600

    Add onClick to display all text and refactor setTimeout to clearTimeout in the onClick

--- src/components/GameText/GameText.tsx
+++ src/components/GameText/GameText.tsx
@@ -8,15 +8,19 @@ interface Props {
 const GameText: React.FC<Props> = ({ text }) => {
   const [textIndex, setTextIndex] = useState<number>(0);
 
+  let timer: ReturnType<typeof setTimeout>
+
   useEffect(() => {
     if (textIndex >= text.length) return;
-    setTimeout(() => {
+    timer = setTimeout(() => {
       setTextIndex(textIndex + 1);
     }, 35);
   }, [textIndex]);
 
+  // send a set update to allow progression in game when text finishes displaying
+
   return (
-    <div>
+    <div onClick={() => {clearTimeout(timer); setTextIndex(text.length)}}>
       <p className="game-text">{text.substring(0, textIndex)}</p>
       <p>{textIndex}</p>
     </div>
