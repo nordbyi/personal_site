34805af4f6b08a3668591213e31f7f9dab07f58a
Ian Nordby <ian.c.nordby@gmail.com>
2023-07-04 15:49:59 -0600

    Add functionality to test sliding animation of CharacterImage when the prop passed to it changes. Simulated by a state change in GameArea on the top-section click

--- src/components/GameArea/GameArea.tsx
+++ src/components/GameArea/GameArea.tsx
@@ -8,17 +8,20 @@ import './GameArea.css'
 const GameArea: React.FC = () => {
   const [win, setWin] = useState<boolean>(false);
   const [loss, setLoss] = useState<boolean>(false);
+  const [showTyping, setShowTyping] = useState<boolean>(false)
+
+  const toggleTypingView = () => {
+    setShowTyping(!showTyping)
+  }
 
   useEffect(() => {
     console.log('loss updated')
   }, [loss])
 
-  const showTyping = true
-
   return (
-  <div className="game-area">
+  <div  className="game-area">
     <CharacterImage source={'https://user-images.githubusercontent.com/108428451/227247013-357061a7-8b34-4cb3-a2e6-f2eaed388a52.gif'} fade={true} slideLeft={showTyping}/>
-    <div className="top-section">
+    <div onClick={toggleTypingView} className="top-section">
       {showTyping &&
       (<div className="typing-space">
         <Typing win={win} loss={loss} setWin={setWin}/>
