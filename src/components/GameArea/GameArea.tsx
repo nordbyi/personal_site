8dac34d0d92ae7b04330711c9cc2eb4b7d856772
Ian Nordby <ian.c.nordby@gmail.com>
2023-07-07 14:34:07 -0600

    Plug in testData into state and component props

--- src/components/GameArea/GameArea.tsx
+++ src/components/GameArea/GameArea.tsx
@@ -6,6 +6,8 @@ import testData from "../../data-model";
 import './GameArea.css'
 
 const GameArea: React.FC = () => {
+  const [progressIndex, setProgressIndex] = useState<number>(0)
+  const [canProgress, setCanProgress] = useState<boolean>(true)
   const [win, setWin] = useState<boolean>(false);
   const [loss, setLoss] = useState<boolean>(false);
   const [showTyping, setShowTyping] = useState<boolean>(false) // replace with data model's value when made
@@ -21,11 +23,11 @@ const GameArea: React.FC = () => {
   return (
   <div  className="game-area">
     <div onClick={toggleTypingView} className="top-section">
-      {showTyping && <Typing win={win} loss={loss} setWin={setWin} time={20} updateGameLoss={setLoss} fade={true}/>}
+      {testData[progressIndex].game && <Typing win={win} loss={loss} setWin={setWin} time={20} updateGameLoss={setLoss} fade={true}/>}
       {/* conditionally render projects */}
-      <CharacterImage source={'https://user-images.githubusercontent.com/108428451/227247013-357061a7-8b34-4cb3-a2e6-f2eaed388a52.gif'} fade={true} slideLeft={showTyping}/>
+      <CharacterImage source={testData[progressIndex].emote} fade={true} slideLeft={showTyping}/>
     </div>
-    <GameText text={'Hi! I\'m Ian! I\'m glad you\'re here! I hope you have a good time and enjoy what I have created! If you see something that needs improvement, let me know!'}/>
+    <GameText text={testData[progressIndex].text}/>
   </div>)
 }
 
