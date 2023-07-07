da161993deaedbfc9f4757285461e9aa41fac0df
Ian Nordby <ian.c.nordby@gmail.com>
2023-07-07 15:06:21 -0600

    Change unmountAnimation property to unmountDelay

--- src/data-model.tsx
+++ src/data-model.tsx
@@ -3,7 +3,7 @@ type step = {
   emote: string,
   game: boolean,
   mountAnimation: boolean,
-  unmountAnimation: boolean,
+  unmountDelay: number,
   lockProgress: boolean,
 }
 
@@ -12,19 +12,19 @@ const testData: step[] = [
     emote: 'https://user-images.githubusercontent.com/108428451/227247013-357061a7-8b34-4cb3-a2e6-f2eaed388a52.gif',
     game: false,
     mountAnimation: false,
-    unmountAnimation: false,
+    unmountDelay: 0,
     lockProgress: false},
   {text: 'This should show the typing game',
     emote: "https://user-images.githubusercontent.com/108428451/227252664-e3826440-001c-4081-89f1-8920b05adb53.gif",
     game: true,
     mountAnimation: true,
-    unmountAnimation: true,
+    unmountDelay: 500,
     lockProgress: true},
   {text: 'Game should be over',
     emote: "https://user-images.githubusercontent.com/108428451/227247885-4f261c10-f361-47fc-8bb6-eef0b2e4161d.gif",
     game: false,
     mountAnimation: false,
-    unmountAnimation: false,
+    unmountDelay: 0,
     lockProgress: false}
 ]
 
