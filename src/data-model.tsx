70a1045084aa736d767884410726a26844923d6a
Ian Nordby <ian.c.nordby@gmail.com>
2023-07-11 10:13:20 -0600

    Add project property to type and another step to data model to show project in future

--- src/data-model.tsx
+++ src/data-model.tsx
@@ -6,6 +6,7 @@ type step = {
   mountAnimation: boolean,
   unmountDelay: number,
   lockProgress: boolean,
+  project: string,
 }
 
 const testData: step[] = [
@@ -14,20 +15,30 @@ const testData: step[] = [
     game: false,
     mountAnimation: false,
     unmountDelay: 0,
-    lockProgress: false},
+    lockProgress: false,
+    project: ''},
   {text: ['This should show the typing game'],
     emote: ["https://user-images.githubusercontent.com/108428451/227252664-e3826440-001c-4081-89f1-8920b05adb53.gif"],
     game: true,
     gameText: 'Test',
     mountAnimation: true,
     unmountDelay: 500,
-    lockProgress: true},
+    lockProgress: true,
+    project: ''},
   {text: ['', 'Nice job! You crushed it!', 'Close one! Better luck next time!'],
     emote: ["https://user-images.githubusercontent.com/108428451/227247885-4f261c10-f361-47fc-8bb6-eef0b2e4161d.gif"],
     game: false,
     mountAnimation: false,
     unmountDelay: 0,
-    lockProgress: false}
+    lockProgress: false,
+    project: ''},
+  {text: ["This will show one of the projects I've created once I get the functionality and component all set up!"],
+    emote: ["https://user-images.githubusercontent.com/108428451/227247885-4f261c10-f361-47fc-8bb6-eef0b2e4161d.gif"],
+    game: false,
+    mountAnimation: false,
+    unmountDelay: 0,
+    lockProgress: false,
+    project: '_______'}
 ]
 
 export default testData
