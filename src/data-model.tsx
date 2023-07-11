effe5639955ccfee8b07553bf54bd6f6915d0a1f
Ian Nordby <ian.c.nordby@gmail.com>
2023-07-11 08:59:12 -0600

    Add conditionals to some of the type properties and add gameWinText and GameLossText as optional properties

--- src/data-model.tsx
+++ src/data-model.tsx
@@ -2,7 +2,9 @@ type step = {
   text: string,
   emote: string,
   game: boolean,
-  gameText: string
+  gameText?: string,
+  gameWinText?: string,
+  gameLossText?: string,
   mountAnimation: boolean,
   unmountDelay: number,
   lockProgress: boolean,
@@ -12,7 +14,6 @@ const testData: step[] = [
   {text: 'Testing 123',
     emote: 'https://user-images.githubusercontent.com/108428451/227247013-357061a7-8b34-4cb3-a2e6-f2eaed388a52.gif',
     game: false,
-    gameText: '',
     mountAnimation: false,
     unmountDelay: 0,
     lockProgress: false},
@@ -26,7 +27,8 @@ const testData: step[] = [
   {text: 'Game should be over',
     emote: "https://user-images.githubusercontent.com/108428451/227247885-4f261c10-f361-47fc-8bb6-eef0b2e4161d.gif",
     game: false,
-    gameText: '',
+    gameWinText: 'Nice job! You crushed it!',
+    gameLossText: 'Close one! Better luck next time!',
     mountAnimation: false,
     unmountDelay: 0,
     lockProgress: false}
