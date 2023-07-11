558bfc3422647c178687d0479a8656831efe9c00
Ian Nordby <ian.c.nordby@gmail.com>
2023-07-11 09:56:39 -0600

    Refactor emote property to be an array of strings much like the text property so that images can be chosen dynamically in future

--- src/data-model.tsx
+++ src/data-model.tsx
@@ -1,6 +1,6 @@
 type step = {
   text: string[], // default text, game win text, game loss text
-  emote: string,
+  emote: string[],
   game: boolean,
   gameText?: string,
   mountAnimation: boolean,
@@ -10,20 +10,20 @@ type step = {
 
 const testData: step[] = [
   {text: ['Testing 123'],
-    emote: 'https://user-images.githubusercontent.com/108428451/227247013-357061a7-8b34-4cb3-a2e6-f2eaed388a52.gif',
+    emote: ['https://user-images.githubusercontent.com/108428451/227247013-357061a7-8b34-4cb3-a2e6-f2eaed388a52.gif'],
     game: false,
     mountAnimation: false,
     unmountDelay: 0,
     lockProgress: false},
   {text: ['This should show the typing game'],
-    emote: "https://user-images.githubusercontent.com/108428451/227252664-e3826440-001c-4081-89f1-8920b05adb53.gif",
+    emote: ["https://user-images.githubusercontent.com/108428451/227252664-e3826440-001c-4081-89f1-8920b05adb53.gif"],
     game: true,
     gameText: 'Test',
     mountAnimation: true,
     unmountDelay: 500,
     lockProgress: true},
   {text: ['', 'Nice job! You crushed it!', 'Close one! Better luck next time!'],
-    emote: "https://user-images.githubusercontent.com/108428451/227247885-4f261c10-f361-47fc-8bb6-eef0b2e4161d.gif",
+    emote: ["https://user-images.githubusercontent.com/108428451/227247885-4f261c10-f361-47fc-8bb6-eef0b2e4161d.gif"],
     game: false,
     mountAnimation: false,
     unmountDelay: 0,
